6c10d4c1a667b7b2b281175e7f01ec11832ad396
Mike Hockerman <mike@hockerman.com>
2015-10-15 00:11:49 -0400

    yay

--- src/app/main.go
+++ src/app/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"runtime"
+	"math/rand"
 	"notion/config"
 	"notion/db"
 	"notion/log"
 	"notion/routes"
+	"runtime"
+	"time"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+	rand.Seed( time.Now().UTC().UnixNano())
 	config.InitEnvs()
 	log.Init()
 	db.Init()
