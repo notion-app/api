b4c7fe9de7c57c228c5bf0323d244be1f09e28d2
Mike Hockerman <mike@hockerman.com>
2015-10-15 00:01:31 -0400

    Fixed response thingys

--- src/notion/middleware/access.go
+++ src/notion/middleware/access.go
@@ -9,17 +9,16 @@ func AccessControl() echo.MiddlewareFunc {
   return func(h echo.HandlerFunc) echo.HandlerFunc {
     return func(c *echo.Context) error {
 
-      if err := h(c); err != nil {
-				c.Error(err)
-			}
-
       if origin := c.Request().Header.Get("Origin"); origin != "" {
-        c.Response().Header().Set("Access-Control-Allow-Origin", origin)
+        c.Response().Header().Set("Access-Control-Allow-Origin", "*")
       }
       c.Response().Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
       c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
       c.Response().Header().Set("Access-Control-Allow-Credentials", "true")
 
+      if err := h(c); err != nil {
+				c.Error(err)
+			}
       return nil
 
     }
