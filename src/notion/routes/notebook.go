8c0456b08d4a523162b10f6a2027e3476c946be8
Mike Hockerman <mike@hockerman.com>
2015-10-26 00:42:26 -0400

    Something

--- src/notion/routes/notebook.go
+++ src/notion/routes/notebook.go
@@ -12,5 +12,20 @@ func GetNotebookNotes(c *echo.Context) error {
 	if err != nil {
 		return err
 	}
+	userFilter := c.Query("user")
+	if userFilter != "" {
+		for _, topic := range notes {
+			remove := []int{}
+			for note_i, _ := range topic.Notes {
+				if topic.Notes[note_i].Owner != userFilter {
+					remove = append(remove, note_i)
+				}
+			}
+			for _, i := range remove {
+				topic.Notes = append(topic.Notes[:i], topic.Notes[i+1:]...)
+			}
+		}
+	}
+	// unjoinedFilter := c.Query("unjoined")
 	return c.JSON(http.StatusOK, notes)
 }
