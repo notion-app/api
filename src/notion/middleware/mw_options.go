6e4b6d5725d07a80a405f1c974ff4c55ecdfb38c
Mike Hockerman <mike@hockerman.com>
2015-10-26 18:18:10 -0400

    Options hack

---
+++ src/notion/middleware/mw_options.go
@@ -0,0 +1,13 @@
+
+package middleware
+
+import (
+  "github.com/gin-gonic/gin"
+  "net/http"
+)
+
+func AcceptOptions(c *gin.Context) {
+  if c.Request.Method == "OPTIONS" {
+    c.AbortWithStatus(http.StatusOK)
+  }
+}
