2e558c45490a796a8a853c1f7745f60bda172d2c
Mike Hockerman <mike@hockerman.com>
2015-11-18 16:01:56 -0500

    Fixed bugs in transform and added test cases

--- src/notion/ot/transform.go
+++ src/notion/ot/transform.go
@@ -7,13 +7,15 @@ func (t Transform) Apply(st string) string {
 	for _, op := range t {
 		switch op.(type) {
 		case int:
-			if op.(int) >= 0 {
+			if op.(int) >= 0 && cursor + op.(int) < len(st) {
 				cursor += op.(int)
-			} else {
-				st = st[:cursor+op.(int)] + st[cursor+1:]
+			} else if op.(int) < 0 && cursor + op.(int) >= 0 {
+				st = st[:cursor+op.(int)] + st[cursor:]
+				cursor += op.(int)
 			}
 		case string:
-			st = st[:cursor] + op.(string) + st[cursor+len(op.(string))+1:]
+			st = st[:cursor] + op.(string) + st[cursor:]
+			cursor += len(op.(string))
 		}
 	}
 	return st
--- src/notion/ot/transform_test.go
+++ src/notion/ot/transform_test.go
@@ -6,5 +6,31 @@ import (
 )
 
 func TestTransformApply(t *testing.T) {
-  
+  xf1 := Transform{1,2,3}
+  r1 := xf1.Apply("hello")
+  if r1 != "hello" {
+    t.Error("XF1 should not modify the string")
+  }
+  xf2 := Transform{1,-1}
+  r2 := xf2.Apply("hello")
+  if r2 != "ello" {
+    t.Error("XF2 should only remove the first character")
+  }
+  xf3 := Transform{2,"xx"}
+  r3 := xf3.Apply("hello")
+  if r3 != "hexxllo" {
+    t.Error("XF3 should add an 'xx' two characters into the string")
+  }
+  xf4 := Transform{
+    1,
+    -1,
+    2,
+    "zz",
+    -1,
+    "xz",
+  }
+  r4 := xf4.Apply("hello")
+  if r4 != "elzxzlo" {
+    t.Error("XF4 did not return the proper response")
+  }
 }
