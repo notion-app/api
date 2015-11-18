b5210b8f87c93fc9a3df07a4a22b578492f258e4
Mike Hockerman <mike@hockerman.com>
2015-11-17 20:41:58 -0500

    Good

--- src/notion/model/database.go
+++ src/notion/model/database.go
@@ -21,7 +21,7 @@ type DbSchoolRequest struct {
 type DbCourse struct {
 	Id       string `db:"id" json:"id"`
 	SchoolId string `db:"school_id" json:"school_id"`
-	Owner 	string `db:"owner" json:"owner"`
+	Owner 	sql.NullString `db:"owner" json:"owner"`
 	Name     string `db:"name" json:"name"`
 	Number   string `db:"number" json:"number"`
 }
